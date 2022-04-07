60a37f5ce8e16321233f982a301e29b4ead06486
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2022-04-07 09:23:53 -0400

    Demo preact bug

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+const values = ["a", "b", "c"];
+
 export default function Home() {
-  return <div></div>
+  return (
+    <label>
+      Pick a value
+      <select defaultValue={values[1]}>
+        {values.map((value) => (
+          <option key={value} value={value}>
+            {value}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
 }
